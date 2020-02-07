84b73d6c0069e9f60af6e4d2812e869cfaf73e7e
Ishara Shanuka <isharashanuka97@gmail.com>
2020-02-07 11:44:50 +0530

    Update Navbar.js

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-    render() { 
-        return ( 
-            <nav>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-         );
-    }
+  static contextType = ThemeContext;
+  render() {
+      console.log(this.context);
+    return (
+      <nav>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
 }
- 
+
 export default Navbar;
